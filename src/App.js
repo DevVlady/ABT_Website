60f05a05c60d94d9e9d3696b3ffc0b2315c23199
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-04-22 13:37:24 -0500

    Added the app.css file with margins and paddings attributes added to eliminate the white spacing.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
 
 function App() {
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,13 +4,7 @@ import { Link as LinkS } from 'react-scroll';
 const Navbar = () => {
     return (
         <div className="nav">
-            <h1>ABT</h1>
-            <div className="nav-links">
-                <LinkS>Home</LinkS>
-                <LinkS>About</LinkS>
-                <LinkS>Services</LinkS>
-                <LinkS>Contact</LinkS>
-            </div>
+            <h1>"ABT" A BARBER'S TOUCH</h1>
         </div>
     );
 }
