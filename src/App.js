de3a1b19c8e722a37852e2800cc31cdf7dc0c7d0
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-04-22 12:35:03 -0500

    Added the react scroll and react router dom packages.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Navbar from './components/Navbar/Navbar.jsx';
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,1 +1,18 @@
 import './Navbar.scss';
+import { Link as LinkS } from 'react-scroll';
+
+const Navbar = () => {
+    return (
+        <div className="nav">
+            <h1>ABT</h1>
+            <div className="nav-links">
+                <LinkS>Home</LinkS>
+                <LinkS>About</LinkS>
+                <LinkS>Services</LinkS>
+                <LinkS>Contact</LinkS>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
